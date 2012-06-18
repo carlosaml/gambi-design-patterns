595ea54bc6ded54e39d0597eeda3021b3698fbb7
Carlos Lopes <carlosaml@gmail.com>
2012-06-17 21:33:17 -0300

    Lone Wolf

---
+++ LoneWolf.java
@@ -0,0 +1,40 @@
+import org.xml.sax.SAXException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.rmi.RemoteException;
+import java.sql.SQLException;
+import java.text.ParseException;
+
+public class LoneWolf {
+
+    public static Object[] process(String file, int size, char type, Object[] status,
+                                     int count, String query, String usr, String pwd,
+                                     String db, String host, int day, int month, int year,
+                                     Object... data)
+            throws NullPointerException, ArrayIndexOutOfBoundsException,
+            OutOfMemoryError, FileNotFoundException, SocketException, MalformedURLException,
+            ArithmeticException, UnknownHostException, SAXException, ParseException,
+            ClassNotFoundException, StackOverflowError, SQLException, IOException,
+            InvocationTargetException, SecurityException, ClassCastException,
+            NoSuchMethodException, NoSuchFieldException, UnsatisfiedLinkError,
+            InstantiationException, ArrayStoreException, IllegalArgumentException,
+            NumberFormatException, AssertionError, InternalError, RemoteException,
+            RuntimeException, Exception, UnknownError, Error, Throwable {
+
+        /*
+          ...
+          ...
+          ...
+          ...
+          ...
+         */
+
+        return null;
+    }
+}
+
